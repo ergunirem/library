30d47e48a1fcdfca260d97251b387c45b961145f
ergunirem <iremthedeveloper@gmail.com>
2020-06-12 13:21:36 +0200

    added modal selectors and functions

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ document.getElementById("submit").addEventListener('click', refreshPage);
 const delButtons = document.querySelectorAll('button');
 delButtons.forEach(delButton => delButton.addEventListener("click", del));
 
+//Get the modal
+const modal = document.getElementById("myModal");
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
 // FUNCTIONS
 function Book(title, author, pages, read) {
     this.title = title;
